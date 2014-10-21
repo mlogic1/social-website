2e2b2fb9b53461373f891521364f9ce449956b0a
mlogic1 <multylogic@gmail.com>
2014-10-21 23:49:37 +0200

    Update
    -fixed password field
    -added redirecting forward if password is good
    -added redirecting back to login if password is bad
    -social.php is the front page if the user logins correctly

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 </head>
 <body>
 
-<div align="center">
+<div align = "center">
 <h2>Please Login</h2>
 <fieldset>
 <legend>Welcome to our social network, Please Login:</legend>
 <form name="login" action="login.php" method="post">
 Username <input type="text" name="username"><br>
-Password <input type="text" name="password"><br>
+Password <input type="password" name="password"><br>
 <input type="submit" value="Log in">
 </fieldset>
 </form>
---
+++ social.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Social Network Front Page</title>
+</head>
+<body>
+<div align = "center">
+<h2>Your front page</h2>
+</div>
+</body>
+</html>
