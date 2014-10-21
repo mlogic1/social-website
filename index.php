5b761a98e7d358f4e8231c505a648c762774b61e
mlogic1 <multylogic@gmail.com>
2014-10-21 23:18:30 +0200

    Login update
    small update

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
 </head>
 <body>
 
-<?php
-echo "Hello";
-?>
+<div align="center">
+<h2>Please Login</h2>
+<fieldset>
+<legend>Welcome to our social network, Please Login:</legend>
+<form name="login" action="login.php" method="post">
+Username <input type="text" name="username"><br>
+Password <input type="text" name="password"><br>
+<input type="submit" value="Log in">
+</fieldset>
+</form>
 
+<?php
+//echo "Hello";
+?>
+</div>
 </body>
 </html>
