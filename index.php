a8dfe744351430c196a5cb4d6de0a0cac54104ef
JediMaster93 <zgblok@gmail.com>
2014-10-22 00:20:06 +0200

    Fixed crappy intendation, radekewn plz.

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <html>
-<head>
-<title>Social Network</title>
-</head>
-<body>
-
-<div align = "center">
-<h2>Please Login</h2>
-<fieldset>
-<legend>Welcome to our social network, Please Login:</legend>
-<form name="login" action="login.php" method="post">
-Username <input type="text" name="username"><br>
-Password <input type="password" name="password"><br>
-<input type="submit" value="Log in">
-</fieldset>
-</form>
-</div>
-</body>
+	<head>
+		<title>Social Network</title>
+	</head>
+		<body>
+			<div align = "center">
+				<h2>Please Login</h2>
+				<fieldset>
+					<legend>Welcome to our social network, Please Login:</legend>
+						<form name="login" action="login.php" method="post">
+							Username <input type="text" name="username"><br>
+							Password <input type="password" name="password"><br>
+							<input type="submit" value="Log in">
+						</form>
+				</fieldset>
+			</div>
+	</body>
 </html>
--- login.php
+++ login.php
@@ -1,20 +1,20 @@
 <html>
-<head>
-<title>Social Network</title>
-</head>
-<body>
-<?php
-$user = $_POST['username'];
-$pwd = $_POST['password'];
-	if($user == 'User1' && $pwd == 'admin'){
-		echo "Login success, Please wait.";
-		header("refresh:2;url=social.php");
-	}else{
-		echo "Bad password, redirecting back to login in 3 seconds";
-		header("refresh:3;url=index.php");
-	}
-?>
+	<head>
+		<title>Social Network</title>
+	</head>
+	<body>
+		<?php
+		$user = $_POST['username'];
+		$pwd = $_POST['password'];
+			if($user == 'User1' && $pwd == 'admin'){
+				echo "Login success, Please wait.";
+				header("refresh:2;url=social.php");
+			}else{
+				echo "Bad password, redirecting back to login in 3 seconds";
+				header("refresh:3;url=index.php");
+			}
+		?>
 
 
-</body>
+	</body>
 </html>
