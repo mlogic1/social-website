999447a5127e2b992d9452595a55298b2efa58da
mlogic1 <multylogic@gmail.com>
2014-10-26 23:57:59 +0100

    CSS Update
    Full update in the TODO.TXT

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <html>
 	<head>
 		<title>Social Network</title>
+		<link rel="stylesheet" type="text/css" href="loginregisterstyle.css"
 	</head>
 		<body>
-			<div align = "center">
-				<h2>Please Login</h2>
-				<fieldset>
-					<legend>Welcome to our social network, Please Login:</legend>
+			<div class="forms" align = "center">
+				<h2 class="header">Please Login</h2>
 						<form name="login" action="login.php" method="post">
-							Username <input type="text" name="username"><br>
-							Password <input type="password" name="password"><br>
-							<input type="submit" value="Log in">
+							<input type="text" name="username" class="inputbox" placeholder = "Username"><br><br>
+							<input type="password" name="password" class="inputbox" placeholder = "Password"><br><br>
+							<input  type="submit" value="Log in" class="submit_button"><br>
+							<a href = "register.php">Not a member yet? Register here</a>
 						</form>
-				</fieldset>
 			</div>
 	</body>
 </html>
---
+++ register.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Social Network</title>
+		<link rel="stylesheet" type="text/css" href="loginregisterstyle.css"
+	</head>
+		<body>
+			<div class="forms" align = "center">
+				<h2 class="header">New User Registration</h2>
+						<form name="registration" action="" method="post">  <!-- add action to register the user -->
+							<input type="text" name="username" class="inputbox" placeholder = "Username"><br><br>
+							<input type="password" name="password" class="inputbox" placeholder = "Password"><br><br>
+							<input type="password" name="password2" class="inputbox" placeholder = "Repeat Password"><br><br>
+							<input type="text" name="firstname" class="inputbox" placeholder = "First Name"><br><br>
+							<input type="text" name="lastname" class="inputbox" placeholder = "Last Name"><br><br>
+							<input  type="submit" value="Complete Registration" class="submit_button"><br>
+							<a href = "index.php">Already a member? Login here</a>
+						</form>
+			</div>
+	</body>
+</html>
