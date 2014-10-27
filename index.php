ef8267773ebc56a5cac04c20311953c3888b0366
mlogic1 <multylogic@gmail.com>
2014-10-27 15:12:26 +0100

    Cosmetic update
    Added another div in index.php as about and links.
    Added focus color in css for inputboxes.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<title>Social Network</title>
+		<meta http-equiv="content-language" content="en">
 		<link rel="stylesheet" type="text/css" href="loginregisterstyle.css"
 	</head>
 		<body>
@@ -11,8 +12,13 @@
 							<input type="password" name="password" class="inputbox" placeholder = "Password"><br><br>
 							<input  type="submit" value="Log in" class="submit_button"><br>
 							<a href = "register.php">Not a member yet? Register here</a><br>
-							<a href = "https://github.com/mlogic1/social-website"><br>Fork this project on GitHub!</a>
 						</form>
+			</div>
+			<br>
+			<div class = "forms" align = "center">
+				<p align = "center">social-website is an open source project for learning purposes. Full source can be found on GitHub.</p>
+				<p align = "center">Check out our <a href="https://github.com/JediMaster93/steam-web-api">Steam web api project</a></p>
+				<p align = "center"><a href = "https://github.com/mlogic1/social-website"><br>Fork this project on GitHub!</a></p>
 			</div>
 	</body>
 </html>
