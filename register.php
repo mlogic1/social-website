1565c4b89e742f9aea48e8f5ca4159236ee3f34c
JediMaster93 <zgblok@gmail.com>
2014-10-29 18:31:18 +0100

    merged database registration into main folder

--- register.php
+++ register.php
@@ -6,14 +6,14 @@
 		<body>
 			<div class="forms" align = "center">
 				<h2 class="header">New User Registration</h2>
-						<form name="registration" action="" method="post">  <!-- add action to register the user -->
+						<form name="registration" action="db.php" method="post">
 							<input type="text" name="username" class="inputbox" placeholder = "Username"><br><br>
 							<input type="password" name="password" class="inputbox" placeholder = "Password"><br><br>
 							<input type="password" name="password2" class="inputbox" placeholder = "Repeat Password"><br><br>
 							<input type="text" name="firstname" class="inputbox" placeholder = "First Name"><br><br>
 							<input type="text" name="lastname" class="inputbox" placeholder = "Last Name"><br><br>
 							<input  type="submit" value="Complete Registration" class="submit_button"><br>
-							<a href = "index.php">Already a member? Login here</a>
+							<!--<a href = "index.php">Already a member? Login here</a>-->
 						</form>
 			</div>
 	</body>
